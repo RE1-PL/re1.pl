17f074b0e4721b2eeb752e68070d8c076629ea15
Dominik Sierechan <108614759+ntds@users.noreply.github.com>
2025-02-12 11:17:38 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,12 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <ul>
-          <li>re1.pl</li>
-          <li>wedevelopwebapps</li>
-          <li>links</li>
+          <li class="text-xl">re1.pl</li>
           <li><a href="https://status.re1.pl/">statuspage</a></li>
           <li><a href="https://blog.re1.pl/">blog</a></li>
           <li><a href="https://github.re1.pl/" target="_blank" rel="noopener noreferrer">github</a></li>
-          <li>email <a href="mailto:ntds@re1.pl">ntds@re1.pl</a></li>
+          <li><a href="https://jenkins.re1.pl/">jenkins ci/cd admin</a></li>
+          <li>contact email <a href="mailto:ntds@re1.pl">ntds@re1.pl</a></li>
           <li>&copy; 2025</li>
         </ul>
       </main>
