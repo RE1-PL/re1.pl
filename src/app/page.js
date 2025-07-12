ffa55b23dea1b048ef300a53fb0c10e5f0651f0b
Dominik <108614759+ntds@users.noreply.github.com>
2025-07-12 21:25:13 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -15,13 +15,16 @@ export default function Home() {
           <li className="text-lg">Applications:</li>
           <li><a href="https://account.re1.pl">manage account <span className="text-red-600">[offline]</span></a></li>
           <li><a href="https://cloud.re1.pl/iot">iot dashboard <span className="text-red-600">[offline]</span></a></li>
+
           <div className="mb-[25px]"></div>
           <li className="text-lg">Experiments:</li>
           <li><a href="https://iot-echo.re1.pl/">request logging api</a></li>
+
           <div className="mb-[25px]"></div>
           <li className="text-lg">Services:</li>
           <li><a href="https://jenkins.524531.xyz/login" target="_blank" rel="noopener noreferrer">jenkins <span className="text-green-600"></span></a></li>
           <li><a href="https://status.re1.pl/">statuspage</a></li>
+
           <div className="mb-[25px]"></div>
           <li className="text-lg">Contact:</li>
           <li><a href="mailto:ntds@re1.pl">ntds@re1.pl</a></li>
@@ -31,13 +34,10 @@ export default function Home() {
           <div className="mb-[25px]"></div>
           <li className="text-lg">Private websites:</li>
           <li><a href="https://524531.xyz/" className="text-blue-600" target="_blank">ntds's blog <span className="text-blue-600"></span></a></li>
-          <li><a href="https://storage.524531.xyz/s/GKdBCRkDmZGMX5G" target="_blank" className="text-gray-200">adam's file share</span></a></li>
+          <li><a href="https://storage.524531.xyz/s/GKdBCRkDmZGMX5G" target="_blank" className="text-gray-200">adam's file share</a></li>
           <li className="text-xs">&copy; 2025</li>
         </ul>
       </main>
-      <div class="fixed bottom-4 right-4 bg-red-600 text-white px-4 py-2 rounded shadow-md text-sm z-50">
-        Services may be offline due to server migration.
-      </div>
     </div>
   );
 }
