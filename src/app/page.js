7af2539ff5654e231d3468923e898920d7bd6c77
Dominik <108614759+ntds@users.noreply.github.com>
2025-07-12 21:06:42 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,8 @@ export default function Home() {
           <li><a href="https://account.re1.pl">manage account <span className="text-red-600">[offline]</span></a></li>
           <li><a href="https://cloud.re1.pl/iot">iot dashboard <span className="text-red-600">[offline]</span></a></li>
           <div className="mb-[25px]"></div>
-          <li><a href="https://iot-echo.re1.pl/iot">request logging api</a></li>
+          <li className="text-lg">Experiments:</li>
+          <li><a href="https://iot-echo.re1.pl/">request logging api</a></li>
           <div className="mb-[25px]"></div>
           <li><a href="https://jenkins.524531.xyz/login" target="_blank" rel="noopener noreferrer">jenkins <span className="text-green-600"></span></a></li>
           <li><a href="https://status.re1.pl/">statuspage</a></li>
@@ -24,7 +25,11 @@ export default function Home() {
           <li className="text-lg">Contact:</li>
           <li><a href="mailto:ntds@re1.pl">ntds@re1.pl</a></li>
           <li><a href="https://github.com/ntds">github.com/ntds</a></li>
+            
+          <div className="mb-[25px]"></div>
+          <li className="text-lg">Private websites:</li>
           <li><a href="https://524531.xyz/" className="text-blue-600" target="_blank">ntds's blog <span className="text-blue-600"></span></a></li>
+          <li><a href="https://storage.524531.xyz/s/GKdBCRkDmZGMX5G" className="text-gray-300" target="_blank"><span className="text-blue-600">adam's file share</span></a></li>
           <li className="text-xs">&copy; 2025</li>
         </ul>
       </main>
